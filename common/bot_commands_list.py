be52e87b6bb60364c2a69b75d870d6a197c40396
sauberr <sauberr10@gmail.com>
2024-12-28 22:43:35 +0300

    Add: File with commands.

--- common/bot_commands_list.py
+++ common/bot_commands_list.py
@@ -1,6 +1,5 @@
 from aiogram.types import BotCommand
 
-
 private = [
     BotCommand(command="/start", description="Launch the bot 🤖"),
     BotCommand(command="/menu", description="Show menu 📋"),
@@ -8,5 +7,4 @@ private = [
     BotCommand(command="/payment", description="Payment options 💰"),
     BotCommand(command="/shipping", description="Delivery options 🚚"),
     BotCommand(command="/orders", description="Your orders 📦"),
-    BotCommand(command="/personal", description="Personal information 📝"),
 ]
