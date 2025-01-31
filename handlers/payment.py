00826567e86504a5e7392379a250cce992d4e2bf
sauberr <sauberr10@gmail.com>
2025-01-31 19:23:37 +0300

    Feat: Add func get crypto rate and convert to rate.

---
+++ handlers/payment.py
@@ -0,0 +1,28 @@
+from typing import Union
+from http import HTTPStatus
+import aiohttp
+
+
+async def get_crypto_rate(crypto: str, fiat: str = 'USD') -> Union[float, None]:
+
+    url: str = f'https://min-api.cryptocompare.com/data/price?fsym={crypto}&tsyms={fiat}'
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == HTTPStatus.OK:
+                    data = await response.json()
+                    rate = data.get(fiat)
+                    if rate is None:
+                        return None
+                    return float(rate)
+    except ConnectionError:
+        return None
+
+
+async def convert_to_crypto(amount: float, fiat: str, crypto: str) -> Union[float, None]:
+    rate = await get_crypto_rate(crypto, fiat)
+    if rate is None:
+        return None
+
+    return amount / rate
